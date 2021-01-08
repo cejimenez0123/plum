823f2cb5ee3a12a481813d696feae2ab3f3f697f
cejimenez0123 <christianjimenez0123@gmail.com>
2021-01-08 10:42:23 -0500

    stepper

---
+++ Development/Swift/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by /fam on 1/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    var body: some View {
+        Stepper(value: $sleepAmount,in: 4...12,step: 0.25) {
+            Text("\(sleepAmount,specifier: "%.3g") hours")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
