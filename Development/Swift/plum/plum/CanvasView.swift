a1b1b15e2f893df96d5f22e170b6970d66f62bc3
cejimenez0123 <christianjimenez0123@gmail.com>
2021-02-27 08:39:15 -0500

    canvas

--- Development/Swift/plum/plum/CanvasView.swift
+++ Development/Swift/plum/plum/CanvasView.swift
@@ -8,70 +8,138 @@
 import SwiftUI
 import PencilKit
 import UIKit
+
+//struct TextView:UIViewRepresentable{
+//
+//    @Binding var text: String
+//    @Binding var textStyle: UIFont.TextStyle
+//
+//
+//
+//
+////    @Binding var width:NSLayoutDimension
+////       func makeUIView(context: Context) -> UITextView {
+////        let textView = UITextView(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100), textContainer: nil)
+////
+////           textView.font = UIFont.preferredFont(forTextStyle: textStyle)
+////           textView.autocapitalizationType = .sentences
+////           textView.isSelectable = true
+////        textView.isScrollEnabled=false
+////           textView.isUserInteractionEnabled = true
+////
+////           return textView
+////       }
+//
+//       func updateUIView(_ uiView: UITextView, context: Context) {
+//           uiView.text = text
+//
+//
+//           uiView.font = UIFont.preferredFont(forTextStyle: textStyle)
+//       }
+////
+//}
+struct Canvas:View{
+   
+    @State var bodyFont: UIFont.TextStyle
+    var body: some View {
+        Color.red
+            .frame(width:300,height:350)
+    }
+    
+   
+};
 struct CanvasView: View {
     @State var color = UIColor.black
       @State var clear = false
-    @State var canvas = PKCanvasView()
+//    @State var canvas = PKCanvasView()
     @State var type : PKInkingTool.InkType = .pencil
     @State var isDraw=true
     @State var colorPicker = false
       var body: some View {
-        NavigationView{
+        
             
-          VStack{
-            PKCanvas(canvas: $canvas, isDraw: $isDraw, color: $color,type: $type, clear:$clear)
-               
-              VStack(){
-                  Button("Change to BLUE"){ self.color = UIColor.blue }
-                  Button("Change to GREEN"){ self.color = UIColor.green }
-                  Button("Clear Canvas"){ self.clear.toggle() }
-              }
-          }
-          .navigationBarItems(trailing: Button(action:{ isDraw.toggle()}){
-            Image(systemName: "pencil.slash")
-                .font(.title)
-          })
-           
                 
+                    ZStack{
+                        Image("transparent")
+                            .resizable()
+                            .ignoresSafeArea()
+                            
+                        HStack(spacing:30){
+                            Spacer()
+                        Canvas(bodyFont:.body)
+                            
+                            ZStack{
+                            
+                            
+                            List{
+                                Image(systemName:"textbox")
+                                Image(systemName: "pencil")
+                            }
+                            }
+                        
+                        
+                    
+                        
+                        
+                
+                
+                
+            }
+           
+//          VStack{
+//            PKCanvas(canvas: $canvas, isDraw: $isDraw, color: $color,type: $type, clear:$clear)
+//
+//              VStack(){
+//                  Button("Change to BLUE"){ self.color = UIColor.blue }
+//                  Button("Change to GREEN"){ self.color = UIColor.green }
+//
+//                  Button("Clear Canvas"){ self.clear.toggle() }
+//              }
+//          }
+//          .navigationBarItems(trailing: Button(action:{ isDraw.toggle()}){
+//            Image(systemName: "pencil.slash")
+//                .font(.title)
+//          })}
         }
+        
       }
     
 }
   
 
-  struct PKCanvas: UIViewRepresentable {
-      class Coordinator: NSObject, PKCanvasViewDelegate {
-          var pkCanvas: PKCanvas
-
-          init(_ pkCanvas: PKCanvas) {
-              self.pkCanvas = pkCanvas
-          }
-      }
-      @Binding var canvas:PKCanvasView
-    @Binding var isDraw: Bool
-      @Binding var color:UIColor
-    @Binding var type:PKInkingTool.InkType
-      @Binding var clear:Bool
-
-      func makeCoordinator() -> Coordinator {
-          Coordinator(self)
-      }
-    var ink:PKInkingTool{
-        PKInkingTool(type,color: color)
-    }
-    let eraser = PKEraserTool(.bitmap)
-      func makeUIView(context: Context) -> PKCanvasView {
-        canvas.drawingPolicy = .anyInput
-        canvas.tool = isDraw ? ink : eraser
-          return canvas
-      }
-
-    func updateUIView(_ canvasView: PKCanvasView, context: Context) {
-        canvasView.tool = isDraw ? ink : eraser
-        
-        }
-
-  }
+//  struct PKCanvas: UIViewRepresentable {
+//      class Coordinator: NSObject, PKCanvasViewDelegate {
+//          var pkCanvas: PKCanvas
+//
+//          init(_ pkCanvas: PKCanvas) {
+//              self.pkCanvas = pkCanvas
+//          }
+//      }
+//      @Binding var canvas:PKCanvasView
+//    @Binding var isDraw: Bool
+//      @Binding var color:UIColor
+//    @Binding var type:PKInkingTool.InkType
+//      @Binding var clear:Bool
+//
+//      func makeCoordinator() -> Coordinator {
+//          Coordinator(self)
+//      }
+//    var ink:PKInkingTool{
+//        PKInkingTool(type,color: color)
+//    }
+//    let eraser = PKEraserTool(.bitmap)
+//      func makeUIView(context: Context) -> PKCanvasView {
+//        canvas.drawingPolicy = .anyInput
+//        canvas.tool = isDraw ? ink : eraser
+//          return canvas
+//      }
+//
+//    func updateUIView(_ canvasView: PKCanvasView, context: Context) {
+//        canvasView.tool = isDraw ? ink : eraser
+//
+//        }
+//
+//  }
 
 struct CanvasView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Development/Swift/plum/plum/Layer.swift
@@ -0,0 +1,20 @@
+//
+//  Layer.swift
+//  plum
+//
+//  Created by /fam on 2/24/21.
+//
+
+import SwiftUI
+
+struct Layer: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+struct Layer_Previews: PreviewProvider {
+    static var previews: some View {
+        Layer()
+    }
+}
