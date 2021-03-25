12936a8e2bd69aee85d631516c86e0817277971e
cejimenez0123 <christianjimenez0123@gmail.com>
2021-03-25 14:25:54 -0400

    table rows

---
+++ Development/Swift/mulp/mulp/CanvasView.swift
@@ -0,0 +1,44 @@
+//
+//  CanvasView.swift
+//  mulp
+//
+//  Created by /fam on 2/25/21.
+//
+
+import SwiftUI
+class ToolView: UITableView{
+    let tools = ["pencil","textarea","photo"]
+    let toolImages = [UIImage]()
+        
+
+   
+    
+    override init(frame: CGRect, style: UITableView.Style){
+        super.init(frame: frame, style: style)
+        for word in tools {
+            toolImages.append(UIImage(systemName: word)!)
+        }
+    }
+   
+    override func numberOfRows(inSection section: Int) -> Int {
+        <#code#>
+        return tools.count
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class CanvasView: UIView {
+    
+   override init(frame: CGRect){
+    super.init(frame: frame)
+    self.backgroundColor = UIColor.white
+ 
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
---
+++ Development/Swift/mulp/mulp/CanvasViewController.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  mulp
+//
+//  Created by /fam on 3/5/21.
+//
+
+import Foundation
+import UIKit
+
+class CanvasViewController: UIViewController{
+    
+    
+    @IBOutlet var TableView: UITableView!
+    
+    
+    override func loadView(){
+    
+//        view = UIView()
+//        let canvas = UIView()
+//        
+//        view.backgroundColor = .gray
+//        canvas.backgroundColor = .white
+//        view.subviews(canvas)
+        
+        
+    }
+}
