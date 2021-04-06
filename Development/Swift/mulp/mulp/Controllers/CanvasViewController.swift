e55e096eaa5b8a85ea67a190c7a35f542b2749ba
cejimenez0123 <christianjimenez0123@gmail.com>
2021-04-06 11:44:28 -0400

    imageboxview

--- Development/Swift/mulp/mulp/Controllers/CanvasViewController.swift
+++ Development/Swift/mulp/mulp/Controllers/CanvasViewController.swift
@@ -32,19 +32,58 @@ class CanvasViewController: UIViewController,UIImagePickerControllerDelegate, UI
             self.dismiss(animated: true, completion: { () -> Void in
 
                     })
-            let imageView = UIImageView(frame: CGRect(x: 20, y: 20, width: pickedImage.size.width, height: pickedImage.size.height))
-            imageView.contentMode = .scaleAspectFit
-            imageView.image = pickedImage
-            view.addSubview(imageView)
+            
+            let inset = UIEdgeInsets(top: 12, left: 10, bottom: 12, right: 10)
+            pickedImage.resizableImage(withCapInsets: inset)
+                            
+           
+            let panGesture = UIPanGestureRecognizer(target: self, action:(Selector(("handleDrag:"))))
+            let imageView = ImageBoxView(image: pickedImage)
+            imageView.addGestureRecognizer(panGesture)
+                view.addSubview(imageView)
             
         }
     }
+   
+    func handleDrag(panGesture: UIPanGestureRecognizer){
+        let translation = panGesture.translation(in: view)
+          guard let gestureView = panGesture.view else {
+            return
+          }
+          gestureView.center = CGPoint(
+            x: gestureView.center.x + translation.x,
+            y: gestureView.center.y + translation.y
+          )
+          panGesture.setTranslation(.zero, in: view)
+        }
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-                dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
             }
     @IBAction func addTextbox(_ sender: UIBarButtonItem) {
     }
-    
-    
 }
-
+    
+class ImageBoxView:UIImageView{
+   
+    override init(image: UIImage?) {
+       
+        
+        let screenSize: CGRect = UIScreen.main.bounds
+        let ws = screenSize.width / image!.size.width
+        let hs = screenSize.height / image!.size.height
+       
+        super.init(image: image)
+        self.contentMode = .scaleAspectFit
+        self.frame = CGRect(x:20,y: 50, width: image!.size.width * ws, height: image!.size.height * hs)
+        let overlayer = UIView(frame: self.frame)
+        overlayer.backgroundColor = UIColor(white: 1, alpha: 0.5)
+        overlayer.layer.isHidden = false
+        overlayer.layer.borderColor = UIColor.gray.cgColor
+        overlayer.layer.borderWidth = 2.0
+        self.addSubview(overlayer)
+    }
+    required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        }
+}
